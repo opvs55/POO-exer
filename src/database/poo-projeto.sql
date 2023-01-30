4aa7596fa662d41abab384abaa1de47401b25511
opvs55 <samvmvniz@gmail.com>
2023-01-30 15:55:35 -0300

    implementado as configurações iniciais

---
+++ src/database/poo-projeto.sql
@@ -0,0 +1,8 @@
+-- Active: 1675103187783@@127.0.0.1@3306
+CREATE TABLE videos(
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    title TEXT NOT NULL,
+    duration NUMBER NOT NULL,
+    create_at TEXT DEFAULT (DATETIME()) NOT NULL
+)
+
